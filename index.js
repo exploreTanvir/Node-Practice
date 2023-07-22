e3ed97f7be94312cc0a4df1d9c420b9913cc2401
Tanvir <tanvirhossen853@gmail.com>
2023-07-22 14:36:47 +0600

    first node

---
+++ index.js
@@ -0,0 +1,19 @@
+var fs=require('fs')
+var http=require('http')
+
+var server=http.createServer(function(req,res){
+
+
+    if(req.url="/"){
+            fs.readFile("index.html",function(error,data){
+                res.writeHead(200,{'content-Type':'text/html'});
+                res.write(data);
+
+                res.end();
+            })
+    }
+
+})
+
+server.listen(4040);
+console.log('server run success');
