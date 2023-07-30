3941650e5112a4b36e01125ea0bfb50022942725
Tanvir Hossen Raju <103171957+exploreTanvir@users.noreply.github.com>
2023-07-30 07:35:18 +0600

    Delete All Data Node To MongoDB

---
+++ Delete many item node to mongodb/DeleteAllItem.js
@@ -0,0 +1,30 @@
+
+const mongoClient=require('mongodb').mongoClient;
+var URL="mongodb+srv://TanvirHossenRaju:TANVIRMTHRRAJUMONGODB@cluster0.vdzppud.mongodb.net/?retryWrites=true&w=majority"
+var config={useUnifiedTopology: true}
+
+mongoClient.connect(URL,config,function(error,MyMongoClient){
+    if(error){
+        console.log("connection error")
+    }
+    else{
+        console.log("connection success")
+        DeleteAllData(MyMongoClient)
+    }
+})
+
+
+function DeleteAllData(MyMongoClient){
+    var MyDatabase=MyMongoClient.db("School")
+    var MyCollection=MyDatabase.collection("Student")
+    MyCollection.deleteOne(MyCollection,function(error,ResultObj){
+        if(error){
+            console.log("All Data Delete failed")
+            
+        }
+        else{
+            console.log("All Data Delete success")
+            console.log(ResultObj.result.n + " Item Deleted")
+        }
+    })
+}
