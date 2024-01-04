825bc4ccbe9576360cc2ae4e32af7a00786a65df
Tanvir Hossen Raju <103171957+exploreTanvir@users.noreply.github.com>
2024-01-04 21:08:27 +0600

    Local module

---
+++ Node_Anisul_Islam/Local-Module/Student.js
@@ -0,0 +1,11 @@
+const getName=()=>{
+    return "Tanvir Hossen Raju"
+}
+const getAge=()=>{
+    return 20
+}
+
+// exports.getName=getName
+// exports.getAge=getAge
+
+module.exports={getAge,getName}
---
+++ Node_Anisul_Islam/Local-Module/getName.js
@@ -0,0 +1,4 @@
+const s1=require("./Student")
+
+console.log(s1.getName())
+console.log(s1.getAge())
